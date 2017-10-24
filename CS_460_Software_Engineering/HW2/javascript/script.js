d272e51928481fb4358e9d2473f340f47010b369
mcalawa <mccalawa@gmail.com>
2017-10-24 14:29:28 -0700

    Fixed the JavaScript

--- CS_460_Software_Engineering/HW2/javascript/script.js
+++ CS_460_Software_Engineering/HW2/javascript/script.js
@@ -1,4 +1,10 @@
-function createTable(statMod, proBonus, modifier, skillName, skillMod) {
+const skillName = ["Acrobatics", "Animal Handling", "Arcana", "Athletics", "Deception", "History", "Insight", "Intimidation", "Investigation", "Medicine", "Nature", "Perception", "Performance", "Persuasion", "Religion", "Slight of Hand", "Stealth", "Survival"];
+
+const skillAbility = [1, 3, 2, 0, 4, 2, 3, 4, 2, 3, 2, 3, 4, 4, 4, 2, 1, 1, 3];
+
+const proBonus = [0, 2, 3, 4, 5, 6];
+
+function skillTable(abilityMod, proMultiplier) {
     "use strict";
     var tab = $("<table>");
     var thead = $("<thead>\
@@ -8,102 +14,120 @@ function createTable(statMod, proBonus, modifier, skillName, skillMod) {
                             <th>Level 5-8</th>\
                             <th>Level 9-12</th>\
                             <th>Level 13-16</th>\
-                            <th>Level 17+</th>\
+                            <th>Level 17-20</th>\
                         </tr>\
                     </thead>");
-    
     thead.appendTo(tab);
     
     var tbody = $("<tbody>");
     var trow = null;
     var tdat = null;
-    var data = null;
-    for(var i = 0; i < modifier.length; i++) {
+    var n = null;
+    for(var ir = 0; ir < 18; ir++) {
         trow = $("<tr>");
-        for(var j = 0; j < 6; j++) {
-            tdat = $("<td>")
-            if(j == 0) {
-                data = skillName[i];
+        for(var ic = 0; ic < 6; ic++) {
+            if(ic === 0) {
+                n = skillName[ir];
             }
             else {
-                var k = skillMod[i];
-                data = statMod[k] + (proBonus[j] * modifier);
+                n = abilityMod[skillAbility[ir]] + (proMultiplier[ir] * proBonus[ic]);
             }
             
-            data.appendTo(tdat);
-            tdat.appendTo(trow);
-            tdat = $("</td>");
+            tdat = $("<td>").html(n);
             tdat.appendTo(trow);
         }
         trow.appendTo(tbody);
-        trow = $("</tr>");
-        trow.appendTo(tbody);
     }
     tbody.appendTo(tab);
-    tbody = $("</tbody></table>")
-    tbody.appendTo(tab);
-    
     return tab;
 }
 
-$("#dndForm").submit(function () {
-    var stats = new Array(5);
-    var statMod = new Array(5);
-    var proBonus = new Array(5);
-    var modifier = new Array(1);
-    var skillName = new Array(1);
-    var skillMod = new Array(1);
+function getStatMod(stat) {
+    var mod = Number;
     
-    stats[0] = $("#str").valueOf; 
-    stats[1] = $("#dex").valueOf;
-    stats[2] = $("#int").valueOf;
-    stats[3] = $("#wis").valueOf;
-    stats[4] = $("#cha").valueOf;
+    if(stat == 1) {
+        mod = -5;
+    }
+    else if(stat < 4) {
+        mod = -4;
+    }
+    else if(stat < 6) {
+        mod = -3;
+    }
+    else if(stat < 8) {
+        mod = -2;
+    }
+    else if(stat < 10) {
+        mod = -1;
+    }
+    else if(stat < 12) {
+        mod = 0;
+    }
+    else if(stat < 14) {
+        mod = 1;
+    }
+    else if(stat < 16) {
+        mod = 2;
+    }
+    else if(stat < 18) {
+        mod = 3;
+    }
+    else if(stat < 20) {
+        mod = 4;
+    }
+    else if(stat < 22) {
+        mod = 5;
+    }
+    else if(stat < 24) {
+        mod = 6;
+    }
+    else if(stat < 26) {
+        mod = 7;
+    }
+    else if(stat < 28) {
+        mod = 8;
+    }
+    else if(stat < 30) {
+        mod = 9;
+    }
+    else {
+        mod = 10;
+    }
+    
+    return mod;
+}
+
+$("#dndForm").submit(function (event) {
+    event.preventDefault();
+    var score = new Array(5);
+    var abilityMod = new Array(5);
+    var proMultiplier = new Array(18);
+    
+    score[0] = $("#str").val();
+    score[1] = $("#dex").val();
+    score[2] = $("#int").val();
+    score[3] = $("#wis").val();
+    score[4] = $("#cha").val();
     
     for(var i = 0; i < 5; i++) {
-        if(stats[i] < 2) {
-            statMod[i] = -5;
-        }
-        else if(stats[i] < 4) {
-            statMod[i] = -4;
-        }
-        else if(stats[i] < 6) {
-            statMod[i] = -3;
-        }
-        else if(stats[i] < 8) {
-            statMod[i] = -2;
-        }
-        else if(stats[i] < 10) {
-            statMod[i] = -1;
-        }
-        else if(stats[i] < 12) {
-            statMod[i] = 0;
-        }
-        else if(stats[i] < 14) {
-            statMod[i] = 1;
-        }
-        else if(stats[i] < 16) {
-            statMod[i] = 2;
-        }
-        else if(stats[i] < 18) {
-            statMod[i] = 3;
-        }
-        else if(stats[i] < 20) {
-            statMod[i] = 4;
+        if(score[i] < 1 || score[i] > 30) {
+            console.log("Invalid ability score of " + score[i]);
+            $("#resultsTable").empty();
+            $("#entryError").html("Please enter valid Ability Scores of between 1 and 30.")
+            return;
         }
         else {
-            statMod[i] = 5;
+            abilityMod[i] = getStatMod(score[i]);
         }
     }
     
-    for(var j = 0; i < 5; i++) {
-        proBonus[j] = j + 2;
-    }
+    for(i = 0; i < 18; i++) {
         
-    modifier[0] = $('input[name="acrobatics"]:checked').valueOf;
-    skillName[0] = "Acrobatics";
-    skillMod[0] = 1;
+        proMultiplier[i] = $('input[name="skill' + i + '"]:checked').val();
+    }
     
+    console.log("Generating skill table");
     $("#resultsTable").empty();
-    $("#resultsTable").append(createTable(statMod, proBonus, modifier, skillName, skillMod));
+    $("#resultsTable").append(skillTable(abilityMod, proMultiplier));
+    $("#entryError").html("");
 });
