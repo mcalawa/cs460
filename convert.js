49c6beb1fe05eb5aea78a8cf4ec0e4b31907e221
mcalawa <mccalawa@gmail.com>
2017-10-27 14:53:23 -0700

    Almost done with all this styling stuff!

--- convert.js
+++
@@ -1,7 +0,0 @@
-var marked = require('marked');
-var fs = require('fs');
-
-var content = fs.readFileSync('./CS_460_Software_Engineering/HW1/content.md', 'utf-8');
-var convertContent = marked(content);
-
-fs.writeFileSync('./CS_460_Software_Engineering/HW1/content.html', convertContent);
