41e1c02f835606b7d8986264d2825344cd24db42
mcalawa <mccalawa@gmail.com>
2017-10-30 12:19:26 -0700

    Working on HW3

---
+++ CS_460_Software_Engineering/HW3/HW3/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*A simple singlely-linked Node class. 
+ * Adapted from Java code written by Scot Morse.*/
+
+namespace Calculator
+{
+    public class Node
+    {
+        public object Data; //the payload
+        public Node Next; //reference to the next Node in the chain
+
+        public Node()
+        {
+            Data = null;
+            Next = null;
+        }
+
+        public Node(object Data, Node Next)
+        {
+            this.Data = Data;
+            this.Next = Next;
+        }
+    }
+}
