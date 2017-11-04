354138763d52a197dd57a074da9d8df1d35572c3
mcalawa <mccalawa@gmail.com>
2017-11-04 16:47:05 -0700

    Working on Feature 1

--- CS_460_Software_Engineering/HW4/Homework4/Homework4/Controllers/HomeController.cs
+++ CS_460_Software_Engineering/HW4/Homework4/Homework4/Controllers/HomeController.cs
@@ -14,8 +14,49 @@ namespace Homework4.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Page1()
         {
+            return View();
+        }
+
+
+        public ActionResult Page1Result()
+        {
+            int nextAge = 0;
+            bool birthdayToday = false;
+            string firstname = Request.Query["firstname"].ToString();
+            ViewBag.Name = firstname;
+
+            DateTime today = DateTime.Today;
+
+            int year = Int32.Parse(Request.Query["year"].ToString());
+            int month = Int32.Parse(Request.Query["month"].ToString());
+            int day = Int32.Parse(Request.Query["day"].ToString());
+
+            DateTime birthdayThisYear = new DateTime(today.Year, month, day);
+
+            if(birthdayThisYear.CompareTo(today) >= 0)
+            {
+                nextAge = today.Year - year;
+
+                if(birthdayThisYear.CompareTo(today) == 0)
+                {
+                    birthdayToday = true;
+                }
+                else
+                {
+                    birthdayToday = false;
+                }
+            }
+            else
+            {
+                nextAge = (today.Year - year) + 1;
+                birthdayToday = false;
+            }
+
+
+
             return View();
         }
     }
