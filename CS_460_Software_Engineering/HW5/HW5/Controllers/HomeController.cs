2d0973dbd3b7bee3be3f15ee1913b4b89f0c724f
mcalawa <mccalawa@gmail.com>
2017-11-05 23:22:11 -0800

    Started HW5

---
+++ CS_460_Software_Engineering/HW5/HW5/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HW5.Models;
+
+namespace HW5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeAddressForm()
+        {
+            return View(new AddressChangeForm());
+        }
+
+        [HttpPost]
+        public ActionResult ChangeAddressForm(AddressChangeForm form)
+        {
+            return View();
+        }
+
+        public ActionResult AddressChanges()
+        {
+            return View();
+        }
+    }
+}
