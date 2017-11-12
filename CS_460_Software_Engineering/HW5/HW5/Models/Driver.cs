6ffa7cac78d4164770dee2309d40ce5a92d3feb9
mcalawa <mccalawa@gmail.com>
2017-11-11 21:11:02 -0800

    Working on HW5

---
+++ CS_460_Software_Engineering/HW5/HW5/Models/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW5.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; } //ID for the driver
+        public DateTime DateOfBirth { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string County { get; set; }
+        public DateTime DateOfChange { get; set; }
+    }
+}
