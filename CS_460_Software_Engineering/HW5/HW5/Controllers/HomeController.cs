9ccc5df25d7367b79cedb6eea52aed725666c217
mcalawa <mccalawa@gmail.com>
2017-11-11 16:26:36 -0800

    Working on HW5

--- CS_460_Software_Engineering/HW5/HW5/Controllers/HomeController.cs
+++ CS_460_Software_Engineering/HW5/HW5/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HW5.Models;
 
 namespace HW5.Controllers
 {
@@ -11,22 +10,6 @@ namespace HW5.Controllers
     {
         // GET: Home
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult ChangeAddressForm()
-        {
-            return View(new AddressChangeForm());
-        }
-
-        [HttpPost]
-        public ActionResult ChangeAddressForm(AddressChangeForm form)
-        {
-            return View();
-        }
-
-        public ActionResult AddressChanges()
         {
             return View();
         }
