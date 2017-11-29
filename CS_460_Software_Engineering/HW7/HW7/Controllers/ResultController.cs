b3afc3490fd0db3d85bcc5ec12584f513fc9c950
mcalawa <mccalawa@gmail.com>
2017-11-28 16:58:21 -0800

    First part of HW7 working

---
+++ CS_460_Software_Engineering/HW7/HW7/Controllers/ResultController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.IO;
+
+namespace HW7.Controllers
+{
+    public class ResultController : Controller
+    {
+        // GET: Result
+        public ActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+        public JsonResult Search()
+        {
+            string key = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyAPIKey"];
+            string limit = Request.QueryString["limit"];
+            string query = Request.QueryString["q"];
+
+            string url = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + query + "&limit=" + limit + "&rating=g&lang=en&fmt=json";
+
+            Console.WriteLine(url);
+
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            string reader = new StreamReader(responseStream).ReadToEnd();
+
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            var results = serializer.DeserializeObject(reader);
+
+            responseStream.Close();
+            response.Close();
+
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CS_460_Software_Engineering/HW7/HW7/Scripts/script.js
@@ -0,0 +1,61 @@
+﻿$("#submitButton").click(search);
+
+function search()
+{
+    var query = $("#searchTerm").val().replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/, "").replace(/\s+/g, "+");
+    var limit = $("#resultNum").val();
+
+    var source = "search?q=" + query + "&limit=" + limit;
+    console.log(source);
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: source,
+        success: displayResults,
+        error: errorOnAjax
+    });
+}
+
+function displayResults(results)
+{
+    var limit = $("#resultNum").val();
+
+    $("#searchResults").empty();
+    $("#searchResults").append(displayGifs(results, limit));
+}
+
+function displayGifs(results, limit)
+{
+    "use strict";
+    var resultContainer = $("<div>");
+    var divContainer = null;
+    var gifImg = null;
+    var bitly = null;
+    var brPoint = $("<br>");
+    if (results.pagination.count < limit)
+    {
+        limit = results.pagination.count;
+    }
+
+    for (var i = 0; i < limit; i++)
+    {
+        divContainer = $("<div>").attr('class', "gifContainer");
+        gifImg = $("<img>").attr('src', results.data[i].images.fixed_width.url);
+        gifImg.appendTo(divContainer);
+        brPoint.appendTo(divContainer);
+        bitly = $("<input>").attr({
+            value: results.data[i].bitly_url,
+            class: "shortUrl"
+        });
+        bitly.appendTo(divContainer);
+        divContainer.appendTo(resultContainer);
+    }
+    console.log(resultContainer);
+    return resultContainer;
+}
+
+function errorOnAjax()
+{
+    console.log("Error");
+}
+
